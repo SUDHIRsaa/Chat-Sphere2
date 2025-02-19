8cf97907638e071e42542c7e101c5a5938d07730
SUDHIRsaa <sudmaurya2611@gmail.com>
2025-02-19 13:08:27 +0530

    Updated .gitignore and removed ignored files

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import authRoutes from "./routes/auth.js"
+import dotenv from "dotenv"
+import messageRoutes from "./routes/message.js"
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
+import cors from "cors"
+import {app,server} from "./lib/socket.js"
+import path from "path"
+
+dotenv.config()
+const PORT= 5002
+const __dirname=path.resolve();
+app.use(cors({
+    origin:'http://localhost:3000',
+   
+    credentials:true,
+    
+}
+
+))
+app.use(cookieParser());
+
+app.use(express.json({ limit: '10mb'}))
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../chatsphere-new/build")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"..chatsphere-new","build","index.html"))
+    })
+}
+
+server.listen(PORT,()=>{
+    console.log("Server is running on port " + PORT)
+    connectDB()
+})
---
+++ backend/src/seeds/user.seed.js
@@ -0,0 +1,115 @@
+import { config } from "dotenv";
+import { connectDB } from "../lib/db.js";
+import User from "../models/user.js";
+
+config();
+
+const seedUsers = [
+  
+  {
+    email: "emma.thompson@example.com",
+    fullname: "Subhodeep Modak",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/1.jpg",
+  },
+  {
+    email: "olivia.miller@example.com",
+    fullname: "Arpit Pandey",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  {
+    email: "sophia.davis@example.com",
+    fullname: "Niraj",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/3.jpg",
+  },
+  {
+    email: "ava.wilson@example.com",
+    fullname: "Sahil TCET",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    email: "isabella.brown@example.com",
+    fullname: "Sujal Pal",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/5.jpg",
+  },
+  {
+    email: "mia.johnson@example.com",
+    fullname: "KOi toh",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+  {
+    email: "charlotte.williams@example.com",
+    fullname: "AMan",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/7.jpg",
+  },
+  {
+    email: "amelia.garcia@example.com",
+    fullname: "Ayush more",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/women/8.jpg",
+  },
+
+  // Male Users
+  {
+    email: "james.anderson@example.com",
+    fullname: "James ",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    email: "william.clark@example.com",
+    fullname: "Willia",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/2.jpg",
+  },
+  {
+    email: "benjamin.taylor@example.com",
+    fullname: " Taylor",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/3.jpg",
+  },
+  {
+    email: "lucas.moore@example.com",
+    fullname: "Moore",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/4.jpg",
+  },
+  {
+    email: "henry.jackson@example.com",
+    fullname: "Henry Jackson",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    email: "alexander.martin@example.com",
+    fullname: "Martin",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/6.jpg",
+  },
+  {
+    email: "daniel.rodriguez@example.com",
+    fullname: "Daniel ",
+    password: "123456",
+    profilePic: "https://randomuser.me/api/portraits/men/7.jpg",
+  },
+];
+
+const seedDatabase = async () => {
+  try {
+    await connectDB();
+
+    await User.insertMany(seedUsers);
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+
+// Call the function
+seedDatabase();
