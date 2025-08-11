cb755687af043375115ae069954315ee7d602525
SUDHIRsaa <sudmaurya2611@gmail.com>
2025-08-11 11:16:42 +0530

    ohk

--- chatsphere-new/src/axios.js
+++ chatsphere-new/src/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5002/api",
   withCredentials: true,
 });
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    console.error("Axios Error:", error);  // Log the error for debugging
-    return Promise.reject(error); // Pass the error for further handling
+    console.error("Axios Error:", error);
+    return Promise.reject(error);
   }
 );
